be37316cca72cfbfb0476aff40237c9c0bcc7d96
Linderjk <slepchenkovia@gmail.com>
2026-04-02 12:23:21 +0300

    feat: add article module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
+import { ArticleModule } from './modules/article/article.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), UserModule, ArticleModule],
   controllers: [AppController],
   providers: [AppService],
 })
