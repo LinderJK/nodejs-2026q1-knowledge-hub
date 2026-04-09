a6046b5b4f02f7e7bd5c670e296bcc715f8be454
Linderjk <slepchenkovia@gmail.com>
2026-04-09 15:21:31 +0300

    fix: delete prisma imports

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
-import { PrismaModule } from 'src/integrations/prisma.module';
 @Module({
-  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
