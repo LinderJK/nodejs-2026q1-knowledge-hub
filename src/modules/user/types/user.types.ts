e4a49e4db17d3e32140a252ce0af8a915c86beed
Linderjk <slepchenkovia@gmail.com>
2026-04-09 15:19:07 +0300

    fix: user types and module

--- src/modules/user/types/user.types.ts
+++ src/modules/user/types/user.types.ts
@@ -1,24 +1,10 @@
-// export interface User {
-//   id: string; // uuid v4
-//   login: string;
-//   password: string;
-//   role?: UserRole;
-//   createdAt: number; // timestamp of creation
-//   updatedAt: number; // timestamp of last update
-// }
-
-import { User } from "generated/prisma/client";
-
-// export enum UserRole {
-//   ADMIN = 'admin',
-//   EDITOR = 'editor',
-//   VIEWER = 'viewer',
-// }
+import { User } from 'generated/prisma/client';
+export { UserRole } from 'generated/prisma/enums';
 
 export type PublicUser = Omit<User, 'password'>;
 
-// export enum UserSortBy {
-//   CREATED_AT = 'createdAt',
-//   UPDATED_AT = 'updatedAt',
-//   ROLE = 'role',
-// }
+export enum UserSortBy {
+  CREATED_AT = 'createdAt',
+  UPDATED_AT = 'updatedAt',
+  ROLE = 'role',
+}
