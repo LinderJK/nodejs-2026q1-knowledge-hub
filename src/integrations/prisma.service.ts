0647321738b1e2c76d45f5a154059a8eed553273
Linderjk <slepchenkovia@gmail.com>
2026-04-09 14:05:11 +0300

    feat: add prisma service

---
+++ src/integrations/prisma.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "generated/prisma/client";
+
+
+
+@Injectable()
+export class PrismaService extends PrismaClient {
+ constructor(private readonly configService: ConfigService) {
+  const adapter = new PrismaPg({
+    connectionString: configService.get('POSTGRES_URL'),
+  });
+  super({
+    adapter,
+  });
+ }
+}
